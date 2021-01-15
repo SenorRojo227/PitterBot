278eebf57ac9dc7b60b494d679a43a6d95947155
Xaaf <xaafcode@gmail.com>
2021-01-15 17:33:44 +0100

    Chore: Initial Commit

---
+++ src/main/java/luxsolis/Pitterbot.java
@@ -0,0 +1,26 @@
+package luxsolis;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class Pitterbot {
+    public static Dotenv dotenv;
+    public static String version = "0.1.0-dev";
+
+    public static void main(String[] arguments) throws Exception {
+        dotenv = Dotenv.load();
+        String botToken = dotenv.get("TOKEN");
+
+        JDA api = JDABuilder.createDefault(botToken)
+                .enableIntents(
+                        GatewayIntent.GUILD_MEMBERS,
+                        GatewayIntent.GUILD_MESSAGES,
+                        GatewayIntent.GUILD_MESSAGE_REACTIONS
+                ).build();
+
+        api.getPresence().setActivity(Activity.playing("Coromon || v" + version));
+    }
+}
