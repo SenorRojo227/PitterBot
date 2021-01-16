b0ca6ce8215b717817d1e090acbbcefedc9679a6
Xaaf <xaafcode@gmail.com>
2021-01-16 18:06:02 +0100

    Chore: Added logging between steps

--- src/main/java/luxsolis/Pitterbot.java
+++ src/main/java/luxsolis/Pitterbot.java
@@ -1,6 +1,7 @@
 package luxsolis;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import luxsolis.utils.Logger;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -12,19 +13,24 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Pitterbot {
     public static Dotenv dotenv;
-    public static String version = "0.1.0-dev";
+    public static String version;
 
     public static void main(String[] arguments) throws Exception {
+        Logger.info("Loading .env", "ENV");
+
         dotenv = Dotenv.load();
         String botToken = dotenv.get("TOKEN");
+        version = dotenv.get("VERSION");
 
+        Logger.info("Building JDA API", "JDA");
         JDA api = JDABuilder.createDefault(botToken)
                 .enableIntents(
-                        GatewayIntent.GUILD_MEMBERS,
                         GatewayIntent.GUILD_MESSAGES,
-                        GatewayIntent.GUILD_MESSAGE_REACTIONS
+                        GatewayIntent.GUILD_MEMBERS
                 ).build();
+        Logger.success("Finished building JDA API", "JDA");
 
-        api.getPresence().setActivity(Activity.playing("Coromon || v" + version));
+        Logger.info("Setting Bot Presence", "BOT");
+        api.getPresence().setActivity(Activity.playing("Coromon! || v" + version));
     }
 }
