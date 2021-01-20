149444f8d85084602d27f2c3ff8555c34b8c3817
William Galiber <wgaliber@gmail.com>
2021-01-20 11:38:08 -0500

    Update Command.java
    Added getParameters() declaration

--- src/main/java/luxsolis/commands/Command.java
+++ src/main/java/luxsolis/commands/Command.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.entities.Message;
  */
 public abstract class Command {
     public abstract String getCommandLabel();
+    public abstract String getParameters();
     public abstract String getDescription();
     public abstract CommandGroup getCommandGroup();
     public abstract void execute(Message message, String[] args);
